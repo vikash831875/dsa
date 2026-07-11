35cd207b471791ab08d6bab33b9968216733c048
vikash831875 <vikashkumarpatel638@gmail.com>
2026-07-11 15:57:18 +0530

    Time: 37 ms (24.64%), Space: 59 MB (74.02%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,50 +2,37 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> result = new ArrayList<>();
-
         Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
+        for(int i =0 ;i<nums.length-2 ; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
 
-            // Skip duplicate first elements
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+            int left = i+1; 
+            int right = nums.length-1; 
 
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(sum == 0){
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
 
-                    // Skip duplicate left values
-                    while (left < right && nums[left] == nums[left - 1]) {
+
+                    while(left<nums.length && nums[left]== nums[left-1]){
                         left++;
                     }
-
-                    // Skip duplicate right values
-                    while (left < right && nums[right] == nums[right + 1]) {
+                    while(right>=0 && nums[right]==nums[right+1]){
                         right--;
                     }
-
-                } else if (sum < 0) {
+                }else if(sum<0){
                     left++;
-                } else {
+                }else{
                     right--;
                 }
             }
-        }
 
-        return result;
+        }
+        return new ArrayList<>(list);
     }
 }
