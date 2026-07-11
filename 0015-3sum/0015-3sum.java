7d0fcc1bc193db72de67c1e8162d03f0d1fec7f6
vikash831875 <vikashkumarpatel638@gmail.com>
2026-07-11 14:02:27 +0530

    Time: 33 ms (73.99%), Space: 59 MB (85.37%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,26 +3,49 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        Set<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> result = new ArrayList<>();
 
         Arrays.sort(nums);
-        for(int i  =0 ; i<nums.length-2; i++){
-            int left  = i+1; 
-            int right = nums.length-1; 
-            while(left<right){
-                int sum = nums[i]+nums[left]+nums[right];
-                if(sum == 0){
-                    set.add(Arrays.asList(nums[i],nums[left],nums[right]));
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
                     left++;
                     right--;
-                }else if(sum<0){
+
+                    // Skip duplicate left values
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicate right values
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
                     left++;
-                }else{
+                } else {
                     right--;
                 }
             }
         }
 
-        return new ArrayList<>(set);
+        return result;
     }
 }
