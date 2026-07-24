804d10e601db335906a233fe0439c38d565b5933
vikash831875 <vikashkumarpatel638@gmail.com>
2026-07-24 10:13:24 +0530

    Time: 3 ms (15.69%), Space: 46.3 MB (73.7%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.java
+++ 3978-unique-middle-element/3978-unique-middle-element.java
@@ -2,8 +2,12 @@ class Solution {
     public boolean isMiddleElementUnique(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for(int  i  =0 ; i<nums.length  ; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
         }
 
         int middle = nums[nums.length / 2];
