6745b2b69a21d0126b713787302c66b92b14a081
vikash831875 <vikashkumarpatel638@gmail.com>
2026-08-11 09:32:02 +0530

    Time: 1 ms (97.52%), Space: 47.3 MB (78.64%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,26 +1,42 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-      int n = nums.length ;
-      int prev[] = new int[n];
-      int suff[] = new int[n];
-      prev[0] = 0;
+    //   int n = nums.length ;
+    //   int prev[] = new int[n];
+    //   int suff[] = new int[n];
+    //   prev[0] = 0;
 
-       for(int i = 1; i<nums.length; i++){
-        prev[i] = prev[i-1]+nums[i-1];
-       }
+    //    for(int i = 1; i<nums.length; i++){
+    //     prev[i] = prev[i-1]+nums[i-1];
+    //    }
 
-       suff[n-1] = 0;
+    //    suff[n-1] = 0;
 
-       for(int i = nums.length-2;i>=0; i--){
-        suff[i] = suff[i+1]+nums[i+1];
-       }
-       int j = 0 ;
-       while(j<prev.length && j<suff.length){
-        if(prev[j]==suff[j]){
-            return j;
+    //    for(int i = nums.length-2;i>=0; i--){
+    //     suff[i] = suff[i+1]+nums[i+1];
+    //    }
+    //    int j = 0 ;
+    //    while(j<prev.length && j<suff.length){
+    //     if(prev[j]==suff[j]){
+    //         return j;
+    //     }
+    //     j++;
+    //    }
+    //    return -1;
+
+    int left =0 ;
+    int sum =0 ;
+    for(int i   = 0  ; i<nums.length; i++){
+        sum= sum+nums[i];
+    }
+
+    for(int i = 0; i<nums.length; i++){
+        int right = sum -left-nums[i];
+        if(left == right){
+            return i ;
         }
-        j++;
-       }
-       return -1;
+
+        left = left+nums[i];
+    }
+   return -1;
     }
 }
