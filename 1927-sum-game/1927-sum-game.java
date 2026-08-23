1b2257595397827431fd174121a24590d9f6c6c4
vikash831875 <vikashkumarpatel638@gmail.com>
2026-08-23 08:53:40 +0530

    Time: 6 ms (97.08%), Space: 46.9 MB (64.23%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int mid = n / 2;
+
+        int diff = 0;
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < mid; i++) {
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                leftQ++;
+            } else {
+                diff += ch - '0';
+            }
+        }
+
+        for (int i = mid; i < n; i++) {
+            char ch = num.charAt(i);
+
+            if (ch == '?') {
+                rightQ++;
+            } else {
+                diff -= ch - '0';
+            }
+        }
+
+        // Same number of '?' on both sides
+        if (leftQ == rightQ) {
+            return diff != 0;
+        }
+
+        // Difference in '?' count is odd
+        if ((leftQ - rightQ) % 2 != 0) {
+            return true;
+        }
+
+        int qDiff = leftQ - rightQ;
+
+        // Extra '?' are on left
+        if (qDiff > 0) {
+            return diff + (qDiff / 2) * 9 != 0;
+        }
+
+        
+        return diff + (qDiff / 2) * 9 != 0;
+    }
+}
