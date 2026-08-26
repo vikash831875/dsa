61447cff5fb5ea6755f2766d0dba13f4f35cdbd5
vikash831875 <vikashkumarpatel638@gmail.com>
2026-08-26 07:52:19 +0530

    Time: 4 ms (16.11%), Space: 45.2 MB (9.4%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,46 +1,57 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         int n = s.length();
-
-        int left = 0;
-        int ones = 0;
-        int minLen = Integer.MAX_VALUE;
+        HashMap<Character, Integer> map = new HashMap<>();
         String ans = "";
+        int minLength = Integer.MAX_VALUE;
+        int left = 0;
 
         for (int right = 0; right < n; right++) {
+            char rch = s.charAt(right);
 
-            if (s.charAt(right) == '1') {
-                ones++;
+            if (map.containsKey(rch)) {
+                map.put(rch, map.get(rch) + 1);
+            } else {
+                map.put(rch, 1);
             }
 
             
-            while (ones > k) {
-                if (s.charAt(left) == '1') {
-                    ones--;
+            while (map.containsKey('1') && map.get('1') > k) {
+                char lch = s.charAt(left);
+
+                map.put(lch, map.get(lch) - 1);
+
+                if (map.get(lch) == 0) {
+                    map.remove(lch);
                 }
+
                 left++;
             }
 
-        
-            if (ones == k) {
+            if (map.containsKey('1') && map.get('1') == k) {
 
-                
                 while (s.charAt(left) == '0') {
+                    char lch = s.charAt(left);
+
+                    map.put(lch, map.get(lch) - 1);
+
+                    if (map.get(lch) == 0) {
+                        map.remove(lch);
+                    }
+
                     left++;
                 }
 
                 int len = right - left + 1;
+                String current = s.substring(left, right + 1);
 
-                if (len < minLen) {
-                    minLen = len;
-                    ans = s.substring(left, right + 1);
-                } 
-                else if (len == minLen) {
-                    String current = s.substring(left, right + 1);
+                if (len < minLength) {
+                    minLength = len;
+                    ans = current;
+                }
 
-                    if (current.compareTo(ans) < 0) {
-                        ans = current;
-                    }
+                if (len == minLength && current.compareTo(ans) < 0) {
+                    ans = current;
                 }
             }
         }
