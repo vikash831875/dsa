89872c163b9f8cc826549ef62e5be72352caf476
vikash831875 <vikashkumarpatel638@gmail.com>
2026-09-16 09:09:04 +0530

    Time: 4 ms (12.21%), Space: 43 MB (91.88%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -6,19 +6,16 @@ class Solution {
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
         int i = 0 ; 
-        int j =0;
+        int j = 0 ;
         while(i<s1.length() && j<s2.length()){
-            if(s1.charAt(i)==s2.charAt(j)){
-                ans = ans+s1.charAt(i);
+            if(s1.charAt(i)== s2.charAt(j)){
+                ans = ans + s1.charAt(i);
             }else{
                 break;
             }
-            
             i++;
             j++;
         }
-
         return ans;
-        
     }
 }
